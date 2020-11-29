84b1bbf4ca51af3ef850a0f23d737364fc0e974a
yack <jacksonkmillsaps@gmail.com>
2020-11-29 15:17:19 -0500

    Get the arrows to be in the right spot when item is selected

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,19 @@
     const prompt = `Put these Bruce Willis Movies in order by the year they were
         released, earliest first.`
 
+    let selected_index = null;
+
+    function select_index(index) {
+        selected_index = selected_index === index
+            ? null : index;
+    }
+
+    $: show_arrow = (index) => {
+        return !(selected_index === null
+            || selected_index === index
+            || selected_index === index - 1);
+    };
+
     const items =  [
         {text: "The Fifth Element", value: 1997},
         {text: "Die Hard", value: 1988},
@@ -13,87 +26,59 @@
     ];
 </script>
 <style>
-    .is-column {
-        display: flex;
-        flex-direction: column;
-        flex-basis: auto;
-        flex-wrap: nowrap;
-        align-items: center;
-    }
     .work-for-long-text {
         height: auto;
         white-space: normal;
     }
-    .button::before {
-
-    }
-    .floater {
-
-    }
-    .holder {
-
-    }
-
 
     .grid-container {
         display: grid;
         grid-template-columns: 1fr 10fr 1fr;
-        row-gap: 1.5em;
+        row-gap: 1.8em;
         column-gap: 0em;
         margin-top: 2.3em;
     }
-    .plus {
+    .arrow {
         top: -2.3em;
         position: relative;
         height: 2.5em;
+        background: rgb(217, 255, 0);
     }
-    .plus::before {
+    .arrow::before {
         width: 0;
         height: 0;
-        border-width: 1.5em 4em;
+        border-width: 1.8em 4em;
         border-style: solid;
         content: '';
         position: absolute;
+        top: -0.5em;
     }
-    .right::before {
+    .arrow.right::before {
         border-color: transparent rgb(217, 255, 0) transparent transparent;
-        right: 0em;
+        right: 1.5em;
     }
-    .left::before {
+    .arrow.left::before {
         border-color: transparent transparent transparent rgb(217, 255, 0);
-        left: 0em;
+        left: 1.5em;
+    }
+    .no-border {
+        border: none;
     }
 </style>
 
 <div class=grid-container>
-    {#each items as {text}}
-        <div class='plus left'></div>
+    {#each items as item, index (item.text)}
+        <div class={show_arrow(index) && 'arrow left'}></div>
 
-        <button class='button is-rounded is-block work-for-long-text'>
-            {text}
+        <button class='button is-rounded is-block work-for-long-text no-border'
+            class:is-primary={selected_index === null || selected_index === index}
+            on:click={() => select_index(index)}>
+            {item.text}
         </button>
 
-        <div class='plus right'></div>
+        <div class={show_arrow(index) && 'arrow right'}></div>
     {/each}
-        <div class='plus left'></div>
+        <div class={show_arrow(items.length) && 'arrow left'}></div>
         <div></div>
-        <div class='plus right'></div>
-</div>
-
-<div class='container'>
-    <div class='notification'>
-        {prompt}
-    </div>
-    <div class='is-column'>
-        {#each items as {text}}
-            <div class=holder>
-            <button class='button is-rounded is-block my-2 work-for-long-text'>
-                {text}
-            </button>
-            <div class=floater>
-                foo
-            </div>
-            </div>
-        {/each}
-    </div>
+        <div class={show_arrow(items.length) && 'arrow right'}></div>
 </div>
